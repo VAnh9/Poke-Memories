5cbccc23a09250ec328463e4e2de8300fcb98ce7
VAnh9 <leleviet207@gmail.com>
2023-07-01 14:11:15 +0700

    commit change

--- src/components/CopyRightScreen.vue
+++ src/components/CopyRightScreen.vue
@@ -1,17 +1,3 @@
 <template>
   <div></div>
 </template>
-<style>
-.copyright {
-  position: fixed;
-  left: 50%;
-  transform: translateX(-50%);
-  bottom: 1.5rem;
-  color: var(--light);
-  z-index: 3;
-  font-size: 1.5rem;
-}
-.copyright a {
-  color: #f4dc26;
-}
-</style>
